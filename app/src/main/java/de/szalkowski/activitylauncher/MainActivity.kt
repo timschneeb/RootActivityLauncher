076fe74f2f3cae05862480e176a87b5b7e6e0a62
ThePBone <tim.schneeberger@outlook.de>
2020-11-23 00:20:01 +0100

    Downgrade to API level 14

--- app/src/main/java/de/szalkowski/activitylauncher/MainActivity.kt
+++ app/src/main/java/de/szalkowski/activitylauncher/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import de.szalkowski.activitylauncher.fragment.AllTasksListFragment
 import de.szalkowski.activitylauncher.fragment.DisclaimerDialogFragment
@@ -38,9 +39,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+            R.id.action_view_disclaimer -> {
+
+                AlertDialog.Builder(this)
+                    .setTitle(getText(R.string.title_dialog_disclaimer))
+                    .setMessage(getText(R.string.dialog_disclaimer))
+                    .setPositiveButton(getText(android.R.string.yes), null)
+                    .show()
+
+                true
+            }
             R.id.action_view_source -> {
                 val i2 = Intent(Intent.ACTION_VIEW)
                 i2.data = Uri.parse(this.getString(R.string.url_source))
+                this.startActivity(i2)
+                true
+            }
+            R.id.action_view_original -> {
+                val i2 = Intent(Intent.ACTION_VIEW)
+                i2.data = Uri.parse(this.getString(R.string.url_original))
                 this.startActivity(i2)
                 true
             }
